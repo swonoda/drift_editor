2bb3e0bb3f31b5aa9beac705ef289224e8d57cdf
Sayo WOnoda <wonoda.sayo@gmail.com>
2025-08-02 01:10:03 +0900

    add config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
-    API_KEY: 'AIzaSyDt6NldgcCJZSDJovG_thLRvjQ8PdkQVes',
-    CLIENT_ID: '750227631397-nbp4fmsl5li9u746g0rvd930hdp0vgb0.apps.googleusercontent.com',
+  CLIENT_ID: "750227631397-nbp4fmsl5li9u746g0rvd930hdp0vgb0.apps.googleusercontent.com",
+  API_KEY: "AIzaSyDt6NldgcCJZSDJovG_thLRvjQ8PdkQVes",
+  APP_ID: "750227631397"
 };
